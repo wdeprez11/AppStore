ad5593219faba335b250a6a42fb350f8b5c3a39f
wdeprez11 <w.deprez11@gmail.com>
2019-04-30 17:48:03 -0500

    Created main file... Hello, World!

---
+++ src/com/deprez/Driver.java
@@ -0,0 +1,14 @@
+package com.deprez;
+
+import javax.swing.*;
+
+public class Driver extends JFrame {
+    public static void main(String[] args) {
+        new Driver().setVisible(true);
+        System.out.println("Hello, World!");
+    }
+
+    private Driver() {}
+
+    private static void quit() {}
+}
