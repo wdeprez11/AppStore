98a9d952df049d06c42862e5ca48d7041a86116e
wdeprez11 <w.deprez11@gmail.com>
2019-04-30 17:58:41 -0500

    Testing for conflicts, also commented out jar inside ignore file.

--- src/com/deprez/Driver.java
+++ src/com/deprez/Driver.java
@@ -5,6 +5,7 @@ import javax.swing.*;
 public class Driver extends JFrame {
     public static void main(String[] args) {
         new Driver().setVisible(true);
+        // Test, checking if merge conflicts occur...
         System.out.println("Hello, World!");
     }
 
