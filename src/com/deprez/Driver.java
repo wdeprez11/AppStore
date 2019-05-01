5b92c1f28d31cf16a00bed6ba0905f4239922de8
wdeprez11 <w.deprez11@gmail.com>
2019-05-01 16:42:34 -0500

    Added a ton of front-end BS, really need to decouple into methods.

--- src/com/deprez/Driver.java
+++ src/com/deprez/Driver.java
@@ -1,16 +1,33 @@
 package com.deprez;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class Driver extends JFrame {
+    private Database database;
+    private Community community;
+    private Store store;
+    private GridBagConstraints gridBagConstraints;
+
     private Driver() {
         super("Hello, World!");
+        database = new Database("jdbc:sqlite:appstore.db");
+        database.connect();
+        community = new Community(database.loadUsers());
+        // store = new Store(database.loadApps());
+        gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 2;
+
+
         setSize(1024, 768);
+        setLayout(new GridLayout(10, 10, 10, 10));
         setLocationRelativeTo(null);
         setResizable(false);
         setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+
         addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent windowEvent) {
@@ -20,14 +37,33 @@ public class Driver extends JFrame {
                 quit();
             }
         });
+
         JLabel jLabel1 = new JLabel("Hello, JFrame!");
+
         add(jLabel1);
+
+        JComboBox<String> jComboBox = new JComboBox();
+
+        JButton jButton = new JButton("JButton");
+        add(jButton, gridBagConstraints);
+
+        for(User user : community.getUsers()) {
+            jComboBox.addItem(user.toString());
+        }
+        add(jComboBox, gridBagConstraints);
+
     }
 
     public static void main(String[] args) {
-        new Driver().setVisible(true);
-        Database database = new Database("jdbc:sqlite:appstore.db");
-        database.connect();
+        Driver driver = new Driver();
+        driver.setVisible(true);
+
+        /*
+        community.addUser("William");
+        community.addUser("George");
+         */
+
+        /*
         database.createTable("user_tb",
                 new String[]{"userId", "userName"},
                 new String[]{"INTEGER", "varchar(50)"},
@@ -35,9 +71,11 @@ public class Driver extends JFrame {
                         {"primary key", "autoincrement"},
                         {"NOT NULL"}
                 });
+         */
     }
 
-    private static void quit() {
+    private void quit() {
+        database.saveUsers(community.getUsers());
         System.exit(0);
     }
 }
