fb9576ac26669511b7f400ddefff41ba42347a94
wdeprez11 <w.deprez11@gmail.com>
2019-05-02 10:23:22 -0500

    Having a heart attack right now. Changed main class in favor of composition as opposed to inheritance.

--- src/com/deprez/Driver.java
+++ src/com/deprez/Driver.java
@@ -5,77 +5,75 @@ import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-public class Driver extends JFrame {
+public class Driver {
     private Database database;
     private Community community;
     private Store store;
     private GridBagConstraints gridBagConstraints;
+    private JFrame jFrame;
 
     private Driver() {
-        super("Hello, World!");
+        jFrame = new JFrame("Hello, World");
         database = new Database("jdbc:sqlite:appstore.db");
         database.connect();
         community = new Community(database.loadUsers());
         // store = new Store(database.loadApps());
-        gridBagConstraints = new GridBagConstraints();
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 2;
 
-
-        setSize(1024, 768);
-        setLayout(new GridLayout(10, 10, 10, 10));
-        setLocationRelativeTo(null);
-        setResizable(false);
-        setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-
-        addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent windowEvent) {
-                // super.windowClosing(windowEvent);
-                // TODO: Save data prior to exiting.
-                dispose();
-                quit();
-            }
-        });
-
-        JLabel jLabel1 = new JLabel("Hello, JFrame!");
-
-        add(jLabel1);
-
-        JComboBox<String> jComboBox = new JComboBox();
-
-        JButton jButton = new JButton("JButton");
-        add(jButton, gridBagConstraints);
-
-        for(User user : community.getUsers()) {
-            jComboBox.addItem(user.toString());
-        }
-        add(jComboBox, gridBagConstraints);
 
     }
 
     public static void main(String[] args) {
         Driver driver = new Driver();
-        driver.setVisible(true);
+        driver.createDatabase();
+        driver.createWindow();
+    }
 
-        /*
-        community.addUser("William");
-        community.addUser("George");
-         */
+    private void createWindow() {
+        jFrame.setSize(1024, 768);
+        jFrame.setLayout(new GridLayout(10, 10, 10, 10));
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setResizable(false);
+        jFrame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        jFrame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent windowEvent) {
+                // super.windowClosing(windowEvent);
+                jFrame.dispose();
+                quit();
+            }
+        });
 
-        /*
+    }
+
+    private void createDatabase() {
         database.createTable("user_tb",
                 new String[]{"userId", "userName"},
-                new String[]{"INTEGER", "varchar(50)"},
+                new String[]{"INTEGER", "VARCHAR(50)"},
                 new String[][]{
-                        {"primary key", "autoincrement"},
+                        {"PRIMARY KEY", "AUTOINCREMENT"},
                         {"NOT NULL"}
                 });
-         */
+        database.createTable("app_tb",
+                new String[]{"appId", "appName", "creatorId"},
+                new String[]{"INTEGER", "VARCHAR(64)", "INTEGER"},
+                new String[][]{
+                        {"PRIMARY KEY", "AUTOINCREMENT"},
+                        {"NOT NULL"},
+                        {}
+                });
+        database.createTable("userapp_tb",
+                new String[]{"appId", "userId", "review"},
+                new String[]{"INTEGER", "INTEGER", "VARCHAR(2000)"},
+                new String[][]{
+                        {},
+                        {},
+                        {}
+                });
     }
 
     private void quit() {
         database.saveUsers(community.getUsers());
+        // database.saveApps(store.getApps());
         System.exit(0);
     }
 }
