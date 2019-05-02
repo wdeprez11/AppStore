367f5c6b6fb330faec2ba5e2f3732874520b7f74
wdeprez11 <w.deprez11@gmail.com>
2019-05-02 14:27:35 -0500

    Reworked file, need to fix bug with recreation of ui when going back and forth.

--- src/com/deprez/Driver.java
+++ src/com/deprez/Driver.java
@@ -2,6 +2,8 @@ package com.deprez;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -10,42 +12,97 @@ public class Driver {
     private Community community;
     private Store store;
     private GridBagConstraints gridBagConstraints;
-    private JFrame jFrame;
+    private JFrame loginJFrame;
+    private JFrame mainJFrame;
 
     private Driver() {
-        jFrame = new JFrame("Hello, World");
         database = new Database("jdbc:sqlite:appstore.db");
         database.connect();
         community = new Community(database.loadUsers());
         // store = new Store(database.loadApps());
 
-
+        loginJFrame = new JFrame("Login");
+        mainJFrame = new JFrame("Main Menu");
     }
 
     public static void main(String[] args) {
         Driver driver = new Driver();
-        driver.createDatabase();
-        driver.createWindow();
+        driver.createLoginWindow();
+        // driver.createDatabase();
+        // driver.createWindow();
     }
 
-    private void createWindow() {
-        jFrame.setSize(1024, 768);
-        jFrame.setLayout(new GridLayout(10, 10, 10, 10));
-        jFrame.setLocationRelativeTo(null);
-        jFrame.setResizable(false);
-        jFrame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-        jFrame.addWindowListener(new WindowAdapter() {
+    private void createLoginWindow() {
+        loginJFrame.setSize(400, 320);
+        loginJFrame.setLayout(new GridLayout(10, 10, 10, 10));
+        loginJFrame.setLocationRelativeTo(null);
+        loginJFrame.setResizable(false);
+        loginJFrame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        loginJFrame.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent windowEvent) {
-                // super.windowClosing(windowEvent);
-                jFrame.dispose();
-                quit();
+                loginJFrame.dispose();
             }
         });
+
+        JLabel userJLabel = new JLabel("Username");
+        loginJFrame.add(userJLabel);
+
+        JTextField userJTextField = new JTextField("", 30);
+        userJTextField.setToolTipText("Enter Username: ");
+        loginJFrame.add(userJTextField);
+
+        JButton loginJButton = new JButton("Login");
+        loginJButton.addActionListener(actionEvent -> {
+            System.out.println("Action: " + actionEvent);
+            loginJFrame.dispose();
+            createMainMenuWindow();
+        });
+        loginJFrame.add(loginJButton);
+
+        JButton quitJButton = new JButton("Quit");
+        quitJButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                System.out.println("Quitting ...");
+                quit(loginJFrame);
+            }
+        });
+        loginJFrame.add(quitJButton);
+
+        loginJFrame.setVisible(true);
+    }
+
+    private void createMainMenuWindow() {
+        mainJFrame.setSize(600, 400);
+        mainJFrame.setLayout(new GridLayout(10, 10, 10, 10));
+        mainJFrame.setLocationRelativeTo(null);
+        mainJFrame.setResizable(false);
+        mainJFrame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        mainJFrame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent windowEvent) {
+                mainJFrame.dispose();
+            }
+        });
+
+        JButton backJButton = new JButton("Logout");
+        backJButton.addActionListener(actionEvent -> {
+            System.out.println("Action" + actionEvent);
+            mainJFrame.dispose();
+            createLoginWindow();
+        });
+        mainJFrame.add(backJButton);
+
+        mainJFrame.setVisible(true);
 
     }
 
     private void createDatabase() {
+        database.createUserTable();
+        // database.createAppTable(); TODO: Finish user table then will get to doing app table.
+        // database.createUserAppTable(); TODO: Link tables together
+        /*
         database.createTable("user_tb",
                 new String[]{"userId", "userName"},
                 new String[]{"INTEGER", "VARCHAR(50)"},
@@ -68,12 +125,14 @@ public class Driver {
                         {},
                         {},
                         {}
-                });
+                });*/
     }
 
-    private void quit() {
+    private void quit(JFrame jFrame) {
         database.saveUsers(community.getUsers());
+        jFrame.dispose();
         // database.saveApps(store.getApps());
+        System.out.println("die");
         System.exit(0);
     }
 }
