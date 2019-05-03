d02d00d453b3ae8482a1bfa67dedfdaaf23ec432
wdeprez11 <w.deprez11@gmail.com>
2019-05-02 19:17:33 -0500

    Updated Store class for easier ArrayList management.

--- src/com/deprez/Store.java
+++ src/com/deprez/Store.java
@@ -22,6 +22,35 @@ public class Store {
         this.apps = apps;
     }
 
+    public void addApp(App app) throws AlreadyExistsException { // TODO
+        if (hasApp(app)) {
+            throw new AlreadyExistsException("App with same appId and appName already exists");
+        } else {
+            apps.add(app);
+        }
+
+    }
+
+    public boolean hasApp(App app) {
+        for (App ap : apps) {
+            if (ap.equals(app)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int size() {
+        return apps.size();
+    }
+
+    @Override
+    public String toString() {
+        return "Store{" +
+                "apps=" + apps +
+                '}';
+    }
+
     // TODO: merge sort
     // TODO: search
 }
