1d4aab1e3491ccc105dd2ff7c3cfd742d17721eb
wdeprez11 <w.deprez11@gmail.com>
2019-05-07 04:17:18 -0500

    Added javadoc to AlreadyExistsException.

--- src/com/deprez/AlreadyExistsException.java
+++ src/com/deprez/AlreadyExistsException.java
@@ -1,6 +1,14 @@
 package com.deprez;
 
+/**
+ * Exception to be throw if an item was found within a unique list.
+ */
 public class AlreadyExistsException extends Exception {
+    /**
+     * Constructor for an AlreadyExistsException.
+     *
+     * @param message The message to send to the exception class
+     */
     public AlreadyExistsException(String message) {
         super(message);
     }
