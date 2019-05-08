53c0751cf1a7a15bb3494bb7f947400e48f9ea31
wdeprez11 <w.deprez11@gmail.com>
2019-05-08 08:25:35 -0500

    More javadoc...

--- src/com/deprez/package-info.java
+++ src/com/deprez/package-info.java
@@ -1,4 +1,18 @@
 /**
- * This is my package
+ * <p>
+ * This package creates a simple database which is used in conjunction
+ * with simple plain old java objects for a simple swing application.
+ * This project is mainly for school, but I also plan to improve on it
+ * once I have learned more skills.
+ * </p>
+ * <p>
+ * I plan to condense the project eventually.
+ * Diagrams ***WILL BE*** included alongside this package description.
+ * </p>
+ * @since 1.0
+ * @see java.awt
+ * @see javax.swing
+ * @see java.sql
+ * @see java.util
  */
 package com.deprez;
